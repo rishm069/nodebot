bf0f1a857fd9abe5f8080a96cb4036c2f352861c
rishm069 <rishm069@protonmail.com>
2019-11-17 23:52:29 +0800

    comment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Telegraf = require('telegraf')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome!'))
+bot.start((ctx) => ctx.reply('Welcome home!'))
 bot.help((ctx) => ctx.reply('Send me a sticker')) 
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
