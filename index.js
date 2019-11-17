6b8a8bd9072ff4de7a4fef76934ba52b20834f17
rishm069 <rishm069@protonmail.com>
2019-11-18 00:33:59 +0800

    comment

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.launch((ctx) => ctx.reply('Welcome!'))
+
+const testMenu = Telegraf.Extra
+  .markdown()
+  .markup((m) => m.inlineKeyboard([
+    m.callbackButton('Test button', 'test')
+  ]))
+
+const aboutMenu = Telegraf.Extra
+  .markdown()
+  .markup((m) => m.keyboard([
+    m.callbackButton('⬅️ Back')
+  ]).resize())
+
+app.hears('test', (ctx) => {
+  ctx.reply('test message', testMenu).then(() => {
+    ctx.reply('about', aboutMenu)
+  })
+})
+
+app.action('test', (ctx) => ctx.answerCallbackQuery('Yay!'))
